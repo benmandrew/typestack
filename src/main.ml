6dc1375913f8a0c063c8818eb6f4e1d712b9471d
benmandrew <benmandrew@gmail.com>
2023-09-30 15:57:37 +0200

    Added print statements and expanded testing

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,8 @@
-type v = N of int | B of bool
-type e = Val of v | Add | Eql | Cond of e * e | Skip | Seq of e * e
-type stack = v list
+open Core
+
+type v = N of int | B of bool [@@deriving compare, sexp_of]
+type e = Val of v | Add | Eql | Cond of e * e | Skip | Print | Seq of e * e
+type stack = v list [@@deriving compare, sexp_of]
 type t = Num | Bool [@@deriving compare]
 type stack_t = t list
 
@@ -9,7 +11,6 @@ module Type_check = struct
 
   let rec infer e s =
     match (e, s) with
-    | _, None -> None
     | Val (N _), Some s -> Some (Num :: s)
     | Val (B _), Some s -> Some (Bool :: s)
     | Add, Some (Num :: Num :: s) -> Some (Num :: s)
@@ -19,6 +20,7 @@ module Type_check = struct
         | Some s0, Some s1 -> if List.equal t_equal s0 s1 then Some s0 else None
         | _, _ -> None)
     | Skip, s -> s
+    | Print, Some (_ :: s) -> Some s
     | Seq (e0, e1), s -> infer e0 s |> infer e1
     | _, _ -> None
 
@@ -33,8 +35,23 @@ module Exec = struct
     | Eql, N n0 :: N n1 :: s -> B (Int.equal n0 n1) :: s
     | Cond (e0, e1), B b :: s -> if b then aux e0 s else aux e1 s
     | Skip, s -> s
+    | Print, v :: s ->
+        let str =
+          match v with
+          | N n -> Printf.sprintf "%d" n
+          | B b -> if b then "true" else "false"
+        in
+        Printf.printf "%s\n" str;
+        s
     | Seq (e0, e1), s -> aux e0 s |> aux e1
-    | _ -> failwith "This should not occur: bug in the typechecker"
+    | _ ->
+        failwith
+          "Stuck configuration, this should not occur: bug in the typechecker?"
 
   let v e = aux e []
 end
+
+let () =
+  let e = Seq (Val (N 0), Seq (Val (N 1), Add)) in
+  if Type_check.v e [] then Exec.v e |> ignore
+  else Printf.printf "Expression does not typecheck"
--- test/test.ml
+++ test/test.ml
@@ -1,15 +1,30 @@
-open Typestack
+open Typestack.Main
 open Ppx_compare_lib.Builtin
 open Sexplib.Std
 
-let%test_unit "Typecheck Completeness" =
-  let open Main in
-  let e = Seq (Val (N 0), Seq (Val (N 1), Add)) in
+let typecheck_test ~expect e =
   let result = Type_check.v e [] in
-  [%test_result: bool] result ~expect:true
+  [%test_result: bool] result ~expect
 
-let%test_unit "Typecheck Soundness" =
-  let open Main in
-  let e = Seq (Val (N 0), Seq (Val (B false), Add)) in
-  let result = Type_check.v e [] in
-  [%test_result: bool] result ~expect:false
+let exec_test ~expect e =
+  if not (Type_check.v e []) then failwith "Expression does not typecheck!";
+  let result = Exec.v e in
+  [%test_result: stack] result ~expect
+
+let%test_unit "typecheck completeness" =
+  let e = Seq (Val (N 0), Seq (Val (N 1), Seq (Add, Print))) in
+  typecheck_test ~expect:true e
+
+let%test_unit "typecheck soundness" =
+  let e = Seq (Val (N 0), Seq (Val (B false), Seq (Add, Print))) in
+  typecheck_test ~expect:false e;
+  let e = Seq (Val (B false), Cond (Val (N 1), Val (B true))) in
+  typecheck_test ~expect:false e
+
+let%test_unit "exec add" =
+  let e = Seq (Val (N 2), Seq (Val (N 1), Add)) in
+  exec_test ~expect:[ N 3 ] e
+
+let%test_unit "exec cond" =
+  let e = Seq (Val (N 9), Seq (Val (B false), Cond (Val (N 3), Val (N 5)))) in
+  exec_test ~expect:[ N 5; N 9 ] e
